a4d465fcf9d5411bcab05827641765a2de0d7b8c
OffLuan <luan031075@gmail.com>
2023-08-25 12:03:40 -0300

    Criação do ambiente de desenvolvimento

---
+++ src/main/java/com/itb/tcc/Ecossis/EcossisApplication.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.Ecossis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcossisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcossisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/tcc/Ecossis/EcossisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.Ecossis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcossisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
